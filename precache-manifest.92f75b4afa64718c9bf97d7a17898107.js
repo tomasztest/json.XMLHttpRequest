e953e021ca9e07e709d78dd4722c02dc7f6f380c
tomasztest <tomaszkotzur@gmail.com>
2019-08-21 10:48:41 +0200

    Updates

---
+++ precache-manifest.92f75b4afa64718c9bf97d7a17898107.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcc9b3293652350f6e8255ee98d5ff47",
+    "url": "/json.XMLHttpRequest/index.html"
+  },
+  {
+    "revision": "28852d0affd3283a1dff",
+    "url": "/json.XMLHttpRequest/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/json.XMLHttpRequest/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "28852d0affd3283a1dff",
+    "url": "/json.XMLHttpRequest/static/js/main.21c341fe.chunk.js"
+  },
+  {
+    "revision": "b5c65897a1b2f112fe41",
+    "url": "/json.XMLHttpRequest/static/js/runtime~main.c403d071.js"
+  }
+]);
